752175fb7cc08defdaa81b95f91431df1ed677ad
Tharindu Mishen <tharindumishen@gmail.com>
2026-09-01 19:32:18 +0530

    Update next.config.js to disable image optimization for production due to Netlify issues

--- next.config.js
+++ next.config.js
@@ -3,5 +3,8 @@ module.exports = {
   reactStrictMode: true,
   images: {
     domains: ["images.unsplash.com"],
+    // Netlify's /_ipx/ image optimizer returns 502 in production;
+    // serve files from /public directly instead.
+    unoptimized: true,
   },
 };
